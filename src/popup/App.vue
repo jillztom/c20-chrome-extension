15f0e62aeb4757cdcd9359fdda2e06aedec0539c
Jilson Thomas <jilson@live.in>
2018-01-30 16:26:33 -0500

    UI tweaks + Exchange ticker

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,22 +1,91 @@
 <template>
   <div class="container" v-if="nav && currencies">
+    <div class="edit-button">
+      <i class="fa fa-gear" v-if="!edit" @click="edit=true"></i>
+    </div>
+    <div class="portfolio-total">
+      <p> My Total Asset Value in Exchange  </p>
+      <h2 class="portfolio">{{ Number(exchangeTotal).toLocaleString('en-US', { style: 'currency', currency: base}) }}</h2>
+      <p> 24hr Change  </p>
+      <h3 :class="{'up': (exchangeInfo.percent_change_24h > 0), 'down': (exchangeInfo.percent_change_24h < 0)}">
+        {{ exchangeInfo.percent_change_24h }}%
+          <i class="fa fa-arrow-up" v-if="exchangeInfo.percent_change_24h > 0"></i>
+          <i class="fa fa-arrow-down" v-if="exchangeInfo.percent_change_24h < 0"></i>
+      </h3>
+    </div>
     
-    <div class="field-area">
-      <div class="area">
-        <h2 class="nav">{{ nav }}</h2>
-        <h4>Nav Price</h4>
+    <div class="asset-table value">
+      <div>
+        <h3>Tokens</h3> 
+        <h4>{{ c20 }}</h4> 
       </div>
-      <div class="area">
-        <h2 class="nav">
-          <input type="text" v-model="c20">
-        </h2>
-        <h4>Tokens</h4>
+      <div> 
+        <h3>NAV Price</h3> 
+        <h4>{{ nav }}</h4> 
       </div>
-      <div class="area">
-        <h2 class="portfolio">{{ Number(total).toLocaleString('en-US', { style: 'currency', currency: base}) }}</h2> 
-        <h4>Portfolio</h4>
+      <div> 
+        <h3>Exchange</h3> 
+        <h4>{{ exchangeRate }}</h4> 
+      </div>
+      <div> 
+        <h3>Asset Value</h3> 
+        <h4>{{ Number(total).toLocaleString('en-US', { style: 'currency', currency: base}) }}</h4> 
       </div>
     </div>
+    
+    <div v-if="edit">
+      <div class="edit-area">
+        <div class="element">
+          <label>Your C20 Tokens</label>
+          <input type="text" v-model="c20">
+        </div>
+        <div class="element">
+          <label>Portfolio Currency</label>
+          <select v-model="base" v-if="currencies">
+            <option v-for="(item, index) in currencies">{{ index }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="button-area">
+        <button type="button" @click.prevent="saveToken()">Save Changes </button>
+        <button type="button" @click.prevent="getC20()">Refresh Stats </button>
+      </div>
+    </div>
+    
+    
+    
+    <!-- 
+    <div class="field-area">
+        <div class="area">
+            <h2 class="nav">{{ nav }}</h2>
+            <h4>Nav Price</h4>
+        </div>
+        <div class="area">
+            <h2 class="portfolio">{{ exchangeRate }}</h2>
+            <h4>Exchange Price</h4>
+        </div>
+    </div>
+    <div class="field-area">
+        <div class="area">
+            <h2 class="nav">
+              <input type="text" v-model="c20">
+            </h2>
+            <h4>Tokens</h4>
+        </div>
+        <div class="area">
+            <h2 class="portfolio">{{ Number(total).toLocaleString('en-US', { style: 'currency', currency: base}) }}</h2> 
+            <h4>Portfolio</h4>
+        </div>
+      
+        <div class="area">
+            <h2 class="portfolio">{{ Number(exchangeTotal).toLocaleString('en-US', { style: 'currency', currency: base}) }}</h2> 
+            <h4>Exchange Value</h4>
+        </div>
+    </div>
+     -->
+    
+    
+    <!-- 
     <hr>
       <div class="movements">
         <div class="items" v-for="(item, index) in movements"> 
@@ -30,7 +99,6 @@
       </div>
     <hr>
     <div class="footer-area">
-      <p>{{ (nav / .95).toFixed(2) * 100  }}% increase from ICO</p> 
       <div
         <button type="button" @click.prevent="saveToken()"><i class="fa fa-floppy-o"></i> </button>
         <button type="button" @click.prevent="getC20()"><i class="fa fa-refresh"></i> </button>
@@ -38,7 +106,7 @@
           <option v-for="(item, index) in currencies">{{ index }}</option>
         </select>
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -50,12 +118,16 @@
         c20: 1500,
         currencies: null,
         movements: null,
+        exchangeRate: 0,
+        exchangeInfo: null,
         base: 'USD',
+        edit : false
       }
     },
     mounted(){
       this.getRates();
       this.getC20();
+      this.getExchangeRate();
     },
     
     watch: {
@@ -67,6 +139,9 @@
     computed:{
       total(){
         return (this.nav * this.c20 * this.currencies[this.base]).toFixed(2);
+      },
+      exchangeTotal(){
+          return (this.exchangeRate * this.c20 * this.currencies[this.base]).toFixed(2);
       }
     },
     
@@ -75,6 +150,13 @@
         axios.get('https://api.fixer.io/latest?base=USD').then((res)=>{
           this.currencies = res.data.rates;
           this.currencies.USD = 1;
+        })
+      },
+      
+      getExchangeRate(){
+        axios.get('https://api.coinmarketcap.com/v1/ticker/c20/').then((res)=>{
+          this.exchangeInfo = res.data[0];
+          this.exchangeRate = res.data[0].price_usd;
         })
       },
       
@@ -97,13 +179,13 @@
       
       saveToken(){
         localStorage.setItem('c20Token', this.c20);
+        this.edit = false;
       },
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
   .movements{
     display: flex;    
     width: 100%;
@@ -141,11 +223,6 @@
   p {
     font-size: 16px;
   }
-  .container{
-    // width: 250px;
-    padding: 10px;
-    background: #f9f9f9;
-  }
   .field-area{
     display: flex;
     justify-content: space-between;
@@ -158,10 +235,6 @@
   }
   h2.nav{
     font-size: 20px;
-    color: #2595ff;
-  }
-  h2.portfolio{
-    color: #6cb305;
   }
   h4{
     text-transform: uppercase;
@@ -189,5 +262,152 @@
     border: 0;
     border-bottom: 1px dashed #ccc;
     background: #fff;
+  }
+  .container{
+    background: #38414d;
+    color: white;
+    padding: 30px;
+    min-width: 250px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  
+  
+  .portfolio-total{
+    text-align: center;
+    margin-bottom: 20px;
+  }
+  .portfolio-total h2{
+    font-size: 22px;
+    font-weight: 600;
+    letter-spacing: 1px;
+  }
+  .portfolio-total h3{
+    padding: 0;
+    margin: 3px 0;
+    font-size: 12px;
+    font-weight: 400;
+  }
+  .portfolio-total h3.down{
+    background: #ff7200;
+    border-radius: 32px;
+    display: inline-block;
+    padding: 1px 8px;
+  }
+  .portfolio-total h3.up{
+    background: #b3e804;
+    border-radius: 32px;
+    display: inline-block;
+    padding: 1px 8px;
+  }
+  .portfolio-total p{
+    font-size: 12px;
+    padding: 0;
+    margin: 5px;
+    color: #dadada54;
+  }
+  
+  
+  
+  .asset-table{
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    padding: 10px 20px;
+    border-radius: 5px;
+    margin: 0 20px;
+    width: 100%;
+  }
+  .asset-table h3{
+    opacity: 1;
+    text-transform: none;
+    margin: 2px 0;
+    font-size: 11px;
+    color: #629bd6;
+  }
+  
+  .asset-table.value h4{
+    font-size: 14px;
+    opacity: 1;
+  }
+  
+  
+  .edit-button{
+    position: absolute;
+    right: 15px;
+    top: 15px;
+  }
+  .edit-button i{
+    font-size: 16px;
+    opacity: 0.3;
+    cursor: pointer;
+    transition: 0.3s all;
+  }
+  .edit-button i:hover{
+    opacity: 1;
+  }
+  
+  .edit-area{
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    border-top: 1px dashed #cccccc3b;
+    padding-top: 20px;
+    margin-top: 10px;
+  }
+  .edit-area input{
+    width: 80%;
+    background: transparent;
+    border: none;
+    font-size: 16px;
+    color: white;
+    border-bottom: 1px dashed #ffffff1f;
+    padding: 2px 0;
+    outline: none;
+  }
+  .edit-area label{
+    margin-bottom: 4px;
+    display: block;
+    opacity: 0.3;
+  }
+  
+  .edit-area .element{
+    margin-bottom: 15px;
+  }
+  .edit-area .element:first-child{
+    margin-right: 10px;
+  }
+  select{
+    background: none;
+    border: none;
+    padding: 0;
+    margin: 0 -7px;
+    color: white;
+    width: 100%;
+    font-size: 16px;
+  }
+  
+  
+  
+  .button-area{
+    display: flex;
+    width: 100%;
+  }
+  button{
+    background: #5e9bd6;
+    border-radius: 3px;
+    padding: 10px;
+    color: white;
+    border: none;
+    outline: none;
+    width: 100%;
+    margin: 0 2px;
+    cursor: pointer;
+    transition: 0.3s all;
+  }
+  button:hover{
+    background: #55acee;
   }
 </style>
